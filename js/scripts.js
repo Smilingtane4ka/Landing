7320b1ea43a1c14f31d97343ab5ac1079b5ad9d6
Smilingtane4ka <smilingtane4ka@gmail.com>
2015-09-10 09:20:04 +0300

    Macbook slides added. Crossbrowser issues fixed

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
         touchEnabled: true,
         adaptiveHeight: true,
         controls: false,
+        pager: false,
         infiniteLoop: false,
         onSliderLoad: function(currentIndex) {
+            $('.mac-content > div').hide();
             $(document).on('mousewheel', function(event) {
                 if (event.deltaY === 0) {
                     return;
@@ -16,6 +18,30 @@ $(document).ready(function() {
                     $slider.goToPrevSlide();
                 }
             });
+        },
+        onSlideNext: function($slideElement, oldIndex, newIndex) {
+            if (newIndex == 1) {
+                $('.mac-container').fadeIn('slow');
+            }
+            if (newIndex == 10) {
+                $('.mac-container').fadeOut('slow');
+            }
+            console.log(oldIndex);
+            console.log(newIndex);
+            if (oldIndex >= 0) {
+                $('.mac-content > div').eq(oldIndex - 1).hide();
+            }
+            $('.mac-content > div').eq(newIndex - 1).slideDown();
+        },
+        onSlidePrev: function($slideElement, oldIndex, newIndex) {
+            if (newIndex == 0) {
+                $('.mac-container').fadeOut('slow');
+            }
+            if (newIndex == 9) {
+                $('.mac-container').fadeIn('slow');
+            }
+            $('.mac-content > div').eq(newIndex - 1).slideDown();
+            $('.mac-content > div').eq(oldIndex - 1).hide();
         }
     });
 });
